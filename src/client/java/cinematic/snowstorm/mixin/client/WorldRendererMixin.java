e489532f7956156deeb731b2728d979c56f8422d
vitaliy <vitaliy60606@gmail.com>
2025-12-07 14:49:20 +0200

    updated to 1.2.0

--- src/client/java/cinematic/snowstorm/mixin/client/WorldRendererMixin.java
+++ src/client/java/cinematic/snowstorm/mixin/client/WorldRendererMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class WorldRendererMixin {
 
     @Inject(method = "renderWeather", at = @At("HEAD"), cancellable = true)
-    private void disableWeatherRendering(FrameGraphBuilder frameGraphBuilder, LightmapTextureManager lightmapTextureManager, Vec3d pos, float tickDelta, Fog fog, CallbackInfo ci) {
+    private void disableWeatherRendering(FrameGraphBuilder frameGraphBuilder, Vec3d pos, float tickDelta, Fog fog, CallbackInfo ci) {
         // Отменяем рендеринг погоды
         ci.cancel();
     }
