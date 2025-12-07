c296899fb32f32018a99649a9a26764bc01984b4
vitaliy <vitaliy60606@gmail.com>
2025-12-07 14:43:46 +0200

    fixed mixin for rain rendering

--- src/client/java/cinematic/snowstorm/mixin/client/WorldRendererMixin.java
+++ src/client/java/cinematic/snowstorm/mixin/client/WorldRendererMixin.java
@@ -1,7 +1,10 @@
 package cinematic.snowstorm.mixin.client;
 
+import net.minecraft.client.render.Fog;
+import net.minecraft.client.render.FrameGraphBuilder;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,9 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class WorldRendererMixin {
 
     @Inject(method = "renderWeather", at = @At("HEAD"), cancellable = true)
-    private void disableWeatherRendering(LightmapTextureManager manager, float tickDelta,
-                                         double cameraX, double cameraY, double cameraZ,
-                                         CallbackInfo ci) {
+    private void disableWeatherRendering(FrameGraphBuilder frameGraphBuilder, LightmapTextureManager lightmapTextureManager, Vec3d pos, float tickDelta, Fog fog, CallbackInfo ci) {
         // Отменяем рендеринг погоды
         ci.cancel();
     }
